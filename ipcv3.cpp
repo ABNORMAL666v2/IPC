d53c6302df52102a9796248a2570d9072c2889e0
Beanos <admin@abby0666.xyz>
2024-06-11 16:51:19 -0700

    test

--- ipcv3.cpp
+++ ipcv3.cpp
@@ -1,9 +1,23 @@
+// ***********************************************************************
+// Assembly         : 
+// Author           : Anon
+// Created          : 03-12-2024
+//
+// Last Modified By : Anon
+// Last Modified On : 06-11-2024
+// ***********************************************************************
+// <copyright file="ipcv3.cpp" company="">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
 #include <iostream>
 #include <string>
 #include <curl/curl.h>
 #include <rapidjson/document.h>
 #include <rapidjson/prettywriter.h>
 #include <rapidjson/stringbuffer.h>
+
 
 size_t write_callback(void* buffer, size_t size, size_t nmemb, std::string* userdata) {
 	size_t totalSize = size * nmemb;
@@ -20,57 +34,63 @@ void print_pretty_json(const std::string& json) {
 	std::cout << sb.GetString() << std::endl;
 }
 
+void ShowIp(const std::string& response)
+{
+	rapidjson::Document d;
+	d.Parse(response.c_str());
+	std::cout << "IP Address: " << d["query"].GetString() << std::endl;
+}
+
 int main(int argc, char* argv[]) {
-	const char* url = "https://if-config.co";
 	const char* ip_api_url = "http://ip-api.com/json";
 	std::string response;
 	long http_code = 0;
-
-	bool pretty_flag = false, json_flag = false;
+	bool pretty_flag = false;
+	bool json_flag = false;
 	for (int i = 1; i < argc; ++i) {
 		if (std::string(argv[i]) == "-pretty") pretty_flag = true;
 		else if (std::string(argv[i]) == "-json") json_flag = true;
 	}
-
-	if (pretty_flag) {
-		CURL* curl = curl_easy_init();
-		curl_easy_setopt(curl, CURLOPT_URL, ip_api_url);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-		curl_easy_perform(curl);
-		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-		if (http_code == 200)
-			print_pretty_json(response);
-		else
-			std::cerr << "HTTP Error: " << http_code << std::endl;
-		curl_easy_cleanup(curl);
+	CURL* curl = curl_easy_init();
+	if (!curl) {
+		std::cerr << "Failed to initialize CURL" << std::endl;
+		return 1;
 	}
-	else if (json_flag) {
-		CURL* curl = curl_easy_init();
-		curl_easy_setopt(curl, CURLOPT_URL, ip_api_url);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-		curl_easy_perform(curl);
-		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-		if (http_code == 200)
+
+	curl_easy_setopt(curl, CURLOPT_URL, ip_api_url);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+	curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+	curl_easy_perform(curl);
+	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+
+	if (http_code == 200) {
+		if (pretty_flag) {
+			print_pretty_json(response);
+			curl_easy_cleanup(curl);
+			return 0;
+		}
+		if (json_flag) {
 			std::cout << response << std::endl;
-		else
-			std::cerr << "HTTP Error: " << http_code << std::endl;
-		curl_easy_cleanup(curl);
+			curl_easy_cleanup(curl);
+			return 0;
+		}
+		if (json_flag == false && pretty_flag == false) {
+			std::cout << "Please provide a flag: -pretty or -json\n" << std::endl;
+			curl_easy_cleanup(curl);
+			return 1;
+		}
+		else {
+			ShowIp(response);
+			curl_easy_cleanup(curl);
+			return 0;
+		}
 	}
 	else {
-		CURL* curl = curl_easy_init();
-		curl_easy_setopt(curl, CURLOPT_URL, url);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-		curl_easy_perform(curl);
-		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-		if (http_code == 200)
-			std::cout << "IP: " << response << std::endl;
-		else
-			std::cerr << "HTTP Error: " << http_code << std::endl;
-		curl_easy_cleanup(curl);
+		std::cerr << "HTTP Error: " << http_code << std::endl;
+		return 1;
 	}
 
+	curl_easy_cleanup(curl);
 	return 0;
 }
---
+++ ipcv3.h
@@ -0,0 +1,14 @@
+// ***********************************************************************
+// Assembly         : 
+// Author           : Anon
+// Created          : 03-24-2024
+//
+// Last Modified By : Anon
+// Last Modified On : 06-11-2024
+// ***********************************************************************
+// <copyright file="ipcv3.h" company="">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+void ShowIp(std::string& response);
